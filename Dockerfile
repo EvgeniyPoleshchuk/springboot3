ef029fb091ed7961bf626f3307f77f5fc0ad8d82
Евгений <2x2great@gmail.com>
2023-09-20 20:18:39 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="Евгений"
+
+ENTRYPOINT ["top", "-b"]
---
+++ src/main/java/netology/config/JavaConfig.java
@@ -0,0 +1,26 @@
+package ru.netology.springbootdemo.config;
+
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.springbootdemo.systemProfile.DevProfile;
+import ru.netology.springbootdemo.systemProfile.ProductionProfile;
+import ru.netology.springbootdemo.systemProfile.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+
+    @Bean
+    @ConditionalOnProperty(prefix ="profile",name = "dev",havingValue = "true",matchIfMissing = true)
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+
+    @Bean
+    @ConditionalOnProperty(prefix = "profile",name = "dev",havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
